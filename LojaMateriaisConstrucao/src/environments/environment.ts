52872d972a893422424d3f2b409a476a00e15e1e
Raphael Muniz <raphaelmunizvarela@gmail.com>
2026-02-08 22:33:16 -0300

    ok

--- LojaMateriaisConstrucao/src/environments/environment.ts
+++ LojaMateriaisConstrucao/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://arrive-exams-cemetery-titanium.trycloudflare.com/api'
+    apiUrl: 'http://localhost:8080/api'
 }
-//apiUrl: 'http://localhost:8080/api'
